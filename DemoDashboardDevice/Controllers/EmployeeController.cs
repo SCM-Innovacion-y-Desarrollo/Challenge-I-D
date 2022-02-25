2d4e414264ffe59442ccf15df2f7d695ee1821f8
Guillermo Godoy Á <guillermo.godoy@scmlatam.com>
2022-02-25 19:22:52 -0300

    Demo

---
+++ DemoDashboardDevice/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using DemoDashboardDevice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDashboardDevice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public List<Person> GetListEmployee()
+        {
+            List<Person> employees = Tools.GetListEmployee(new List<Person>());
+            return employees;
+        }
+    }
+}
